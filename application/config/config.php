f11bb539d16ea78b23b41366383c67332b7ccd3c
ed <emsalaysay@newalchemysolutions.com>
2019-12-08 21:44:00 +0800

    modified config

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,6 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+    ob_start();
 
 /*
 |--------------------------------------------------------------------------
